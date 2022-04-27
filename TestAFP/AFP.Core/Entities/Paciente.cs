585f68d2879c6fd17fa8adbbd2cd9c863174685e
aperez <aperez@integra.international>
2022-04-27 12:40:16 -0600

    Arquitecture done
    Core, Infrasctructure, api and web.

---
+++ TestAFP/AFP.Core/Entities/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace AFP.Core.Entities
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public bool Estado { get; set; }
+        public DateTime Fecha_Registro { get; set; }
+    }
+}
